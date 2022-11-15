39ecb2c10ac885973b3bb6fb4d9e2b03a166bbbc
jerobe <jerobejrobinson@gmail.com>
2022-11-15 07:01:31 -0600

    added commits for API Response Time/Limit

--- pages/api/generateWorkOrder.js
+++ pages/api/generateWorkOrder.js
@@ -1,9 +1,13 @@
 import createPDF from "utils/pdf/createWorkOrders";
 export default async function handler(req, res) {
+    // work on retrieving a base number from mongoDB
     const {qty, base} = JSON.parse(req.body);
     const img = await fetch('https://ucarecdn.com/1d09084a-478a-4610-ad5e-ac3deb91ecd8/logo.PNG');
     const imgBuffer = Buffer.from(await img.arrayBuffer())
     res.setHeader('Content-Type', 'application/pdf')
     res.setHeader('Content-Disposition', 'attachment; filename=workorder.pdf')
+
+    // API response limit of 4mb. Can override though. 100 pages clocks at 3.72MB
+    // Next JS API routes are meant for quick responses 
     res.send(await createPDF(imgBuffer, Number(qty), Number(base)))
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useState } from "react"
 export default function Home() {
-  const [blobUrl, setBlobUrl] = useState()
+  const [blobUrl, setBlobUrl] = useState('')
+  const [qty, setQty] = useState()
+  const [base, setBase] = useState()
   const generateWorkOrders = (data) => {
+    setBlobUrl('loading')
     fetch(`/api/generateWorkOrder`, {
       method: 'post',
       body: JSON.stringify({qty: data.qty, base: data.base})
@@ -14,21 +17,19 @@ export default function Home() {
     })
 
   }
-  const [qty, setQty] = useState('0')
-  const [base, setBase] = useState('0')
   return (
     <div>
       <div>
-        <label htmlFor="qty" className="font-bold">Quanity:</label>< br/>
-        <input type="number" name="qty" id="qty" defaultValue={0} onChange={(e) => { setQty(e.target.value)}}/>
+        <label htmlFor="qty" className="font-bold">Enter Quanity:</label>< br/>
+        <input type="number" name="qty" id="qty" onChange={(e) => { setQty(e.target.value)}}/>
       </div>
       <div>
-        <label htmlFor="base" className="font-bold">Starting Number:</label>< br/>
-        <input type="number" name="base" id="base" defaultValue={0} onChange={(e) => { setBase(e.target.value)}}/>
+        <label htmlFor="base" className="font-bold">Enter Starting Number:</label>< br/>
+        <input type="number" name="base" id="base" onChange={(e) => { setBase(e.target.value)}}/>
       </div>
       <button onClick={() => generateWorkOrders({qty, base})} className="p-4 rounded bg-blue-500 text-white font-bold text-lg">Generate Work Orders</button>
       <div>
-        {blobUrl ? (<a href={blobUrl}> View Work Orders</a>) : ''}
+        {blobUrl != 'loading' && blobUrl != '' ? (<a href={blobUrl}> View Work Orders</a>) : blobUrl == 'loading' ? 'loading' : '' }
       </div>
     </div>
   )
