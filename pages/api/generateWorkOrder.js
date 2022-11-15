baa5379f8c82f4aa9fff1f902e671d039327518b
jerobe <jerobejrobinson@gmail.com>
2022-11-15 05:56:20 -0600

    pulling image from uploadcare transform into buffer

--- pages/api/generateWorkOrder.js
+++ pages/api/generateWorkOrder.js
@@ -1,7 +1,8 @@
-import PDFDocument from "pdfkit"
 import createPDF from "utils/pdf/createWorkOrders";
 export default async function handler(req, res) {
     const {qty, base} = JSON.parse(req.body);
-    
-    createPDF(Number(qty), Number(base), res)
+    const img = await fetch('https://ucarecdn.com/1d09084a-478a-4610-ad5e-ac3deb91ecd8/logo.PNG');
+    const imgBuffer = Buffer.from(await img.arrayBuffer())
+    console.log(imgBuffer)
+    createPDF(imgBuffer, Number(qty), Number(base), res)
 }
--- utils/pdf/createWorkOrders.js
+++ utils/pdf/createWorkOrders.js
@@ -2,9 +2,9 @@ import fs from "fs";
 const PDFDocument = require('pdfkit');
 const JsBarcode = require('jsbarcode');
 const { createCanvas } = require('canvas');
-var concat = require('concat-stream')
-const imageBuffer = fs.readFileSync("public/logo.png");
-const createPDF = (qty, base, res) => {
+const concat = require('concat-stream')
+
+const createPDF = (imgBuffer, qty, base, res) => {
     let whBarcode = createCanvas();
     let woBarcode = createCanvas();
 
@@ -23,7 +23,7 @@ const createPDF = (qty, base, res) => {
         JsBarcode(whBarcode, "wh300");
         JsBarcode(woBarcode, `${base + x }`);
         doc.image(whBarcode.toDataURL(), 41.4, 10.8, {width: 81.4});
-        doc.image(imageBuffer, 146.9, 2.9, {width: 58})
+        doc.image(imgBuffer, 146.9, 2.9, {width: 58})
         doc.font('Helvetica-Bold');
         doc.fontSize(15.6)
         doc.text('FUEL SHOP REPAIR ORDER', 216, 9.2, {
