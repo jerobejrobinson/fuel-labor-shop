60bc190b7bafed1a96ca2cad9eb60fa94ba8b1c1
jerobe <jerobejrobinson@gmail.com>
2022-11-15 06:39:21 -0600

    updated package.json

--- pages/api/generateWorkOrder.js
+++ pages/api/generateWorkOrder.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
     const {qty, base} = JSON.parse(req.body);
     const img = await fetch('https://ucarecdn.com/1d09084a-478a-4610-ad5e-ac3deb91ecd8/logo.PNG');
     const imgBuffer = Buffer.from(await img.arrayBuffer())
-    console.log(imgBuffer)
-    createPDF(imgBuffer, Number(qty), Number(base), res)
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'attachment; filename=workorder.pdf')
+    res.send(await createPDF(imgBuffer, Number(qty), Number(base)))
 }
